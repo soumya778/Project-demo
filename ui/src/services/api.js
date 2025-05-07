71ac33501a52287e39e2129b50ff1ddf7a8a3da3
soumya <21cse121@gweca.ac.in>
2025-05-08 00:09:25 +0530

    Update API URL to live backend

--- ui/src/services/api.js
+++ ui/src/services/api.js
@@ -1,7 +1,23 @@
+// import axios from 'axios';
+
+// const API = axios.create({
+//   baseURL: 'http://localhost:5000/api', // Backend server URL
+// });
+
+// API.interceptors.request.use((req) => {
+//   const token = localStorage.getItem('token');
+//   if (token) {
+//     req.headers.Authorization = token;
+//   }
+//   return req;
+// });
+
+// export default API;
+
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:5000/api', // Backend server URL
+  baseURL: 'https://suggestify-backend-n61e.onrender.com/api', // Live backend server URL
 });
 
 API.interceptors.request.use((req) => {
